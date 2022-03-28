de90404ece17a6f7b8540953ee7bf068f62b8e3a
Bragin Vlad <dalvelab@gmail.com>
2022-03-28 20:00:30 +0500

    feat(sr-admin-app): Remove Contacts api, modify controller for Item

--- src/api/item/controllers/item.js
+++ src/api/item/controllers/item.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  *  item controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::item.item');
+module.exports = createCoreController("api::item.item", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service("api::item.item").find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
